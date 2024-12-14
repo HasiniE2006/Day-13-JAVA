a56968513236252a0a6e5ac554036cebc6dbff6e
Hasini E <hasini1530@gmail.com>
2024-12-14 01:32:01 -0800

    Add files via upload

---
+++ try_catch_finally_blocks.java
@@ -0,0 +1,25 @@
+public class try_catch_finally_blocks
+{
+  public static void main(String args[])
+  {
+    //try-catch
+    try{
+      int d=90/0;
+    }
+    catch(ArithmeticException v)
+    {
+      System.out.println("Bad Request");
+    }
+    try{
+      int d=90/0;
+    }
+    catch(ArithmeticException v)
+    {
+      System.out.println("Catch block is running");
+    }
+    finally
+    {
+      System.out.println("Finally is running");
+    }
+  }
+}
