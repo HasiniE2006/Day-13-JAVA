b4fe3513e3ad099e8b644dbe5675f01139a4a489
Hasini E <hasini1530@gmail.com>
2024-12-14 11:21:47 +0530

    Add files via upload

---
+++ Super_key.java
@@ -0,0 +1,22 @@
+class A
+{
+public void r()
+{
+    System.out.println("Hello");
+}
+}
+class b extends A
+{
+public void r1()
+{
+    super.r();
+    System.out.println("Hii");
+}
+}
+public class Super_key{
+    public static void main(String args[])
+    {
+        b n=new b();
+        n.r1();
+    }
+}
